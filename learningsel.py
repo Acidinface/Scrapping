6593d9bad1dac53a5bda043d1894661a73b5f2f3
Oleksandr Kapitonov <sit4everacab@gmail.com>
2024-05-29 16:26:58 +0300

    modified selenium code

--- learningsel.py
+++ learningsel.py
@@ -1,3 +1,9 @@
 from selenium import webdriver
 
-website = ''
+website = 'https://www.adamchoi.co.uk/overs/detailed'
+driver_path = r'Users/Scrapping/chromedriver-win64/chromedriver'
+
+driver = webdriver.Chrome(driver_path)
+response = driver.get(website)
+
+
