722a6fda0c84576edecbb94c0a4b2000d9090727
Acidinface <sit4everacab@gmail.com>
2024-06-16 12:57:20 +0300

    found title

--- learningscrapy/learningScrapy/spiders/audiblespider.py
+++ learningscrapy/learningScrapy/spiders/audiblespider.py
@@ -7,9 +7,9 @@ class AudiblespiderSpider(scrapy.Spider):
     start_urls = ["https://www.audible.co.uk/search"]
 
     def parse(self, response):
-        book_list = response.xpath('//div[@id="center-3"]/div/div/div/span/ul/li')
+        book_list = response.xpath('//div[@class="adbl-impression-container "]/div/span/ul/li')
         for book in book_list:
-            title = book.xpath('.//h3/text()').get()
+            title = book.xpath('.//h3/a/text()').get()
             author = book.xpath('.//li[contains(@class, "author")]/text()').getall()
             break
             narrator = book.xpath('.//li[contains(@class, "narrator")]/text()').getall()
@@ -20,15 +20,16 @@ class AudiblespiderSpider(scrapy.Spider):
             rating = book.xpath('.//li[contains(@class, "rating")]/text()').getall()
             popular = book.xpath('.//li[contains(@class, "mostPopular")]/text()').getall()
             trending = book.xpath('.//li[contains(@class, "trending")]/text()').getall()
-            yield {
-                'title': title,
-                'author': author,
-                'narrator': narrator,
-                'series': series,
-                'length': length,
-                'release': release,
-                'language': language,
-                'rating': rating,
-                'popular': popular,
-                'trending': trending,
-            }
+
+        yield {
+            'title': title,
+            'author': author,
+            # 'narrator': narrator,
+            # 'series': series,
+            # 'length': length,
+            # 'release': release,
+            # 'language': language,
+            # 'rating': rating,
+            # 'popular': popular,
+            # 'trending': trending,
+        }
