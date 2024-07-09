05ea9f245c366149aed633e5566dc6885ffdb8d3
Acidinface <sit4everacab@gmail.com>
2024-07-09 15:32:46 +0300

    opened pipeline

--- pet/jobscraper/jobscraper/settings.py
+++ pet/jobscraper/jobscraper/settings.py
@@ -93,9 +93,9 @@ DOWNLOAD_TIMEOUT = 10
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "jobscraper.pipelines.JobscraperPipeline": 300,
-#}
+ITEM_PIPELINES = {
+   'jobscraper.pipelines.JobscraperPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
